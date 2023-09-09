802ca808588f7ffed7ff90b4c0ba2350d311734f
yajas-menon <you@example.com>
2023-09-09 11:01:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import Aos from "aos";
+import "./App.css";
+
+import Header from "./assets/components/Header/Header";
+import Footer from "./assets/components/Footer/Footer";
+import Hero from "./assets/components/UI/Hero";
+import Services from "./assets/components/UI/Services";
+import Portfolio from './assets/components/UI/Portfolio';
+import Contact from "./assets/components/UI/Contact";
+
 
 function App() {
+  useEffect(() => {
+    Aos.init();
+  },[]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <main>
+        <Hero />
+        <Services />
+        <Portfolio />
+        <Contact />
+      </main>
+      <Footer />
+    </>
   );
 }
 
