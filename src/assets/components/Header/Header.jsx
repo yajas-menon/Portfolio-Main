a827b8de3239e8979fb8ab34f7ebcc609b681a53
Yajas Menon <83765967+yajas-menon@users.noreply.github.com>
2024-02-22 15:27:31 +0530

    Update Header.jsx

--- src/assets/components/Header/Header.jsx
+++ src/assets/components/Header/Header.jsx
@@ -32,7 +32,7 @@ const Header = () => {
           </div>
           
           <div className='flex items-center gap-4'>
-            <a target ='_blank' href= 'https://drive.google.com/file/d/1YVxakz7rFaDQF_rlosYq21pP46d0SFUE/view?usp=drivesdk' download='Resume'><button className=' md:flex md:items-center mx-4 my-6 md:my-0 flex items-center gap-2 text-white font-[600] border border-solid
+            <a target ='_blank' href= 'https://drive.google.com/file/d/1YVxakz7rFaDQF_rlosYq21pP46d0SFUE/view?usp=drive_link' download='Resume'><button className=' md:flex md:items-center mx-4 my-6 md:my-0 flex items-center gap-2 text-white font-[600] border border-solid
             border-smallTextColor py-2 px-4 rounded-[8px] max-h-[40px] hover:bg-smallTextColor hover:text-white
             hover:font-[500] ease-in duration-200'><i class="ri-mail-download-line"></i>Resume</button></a>
             <span onClick={toggleMenu} className='text-2xl text-smallTextColor md:hidden cursor-pointer'>
